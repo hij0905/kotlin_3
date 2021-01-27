e44269f75a98f6ce187beceb6d36c780f7944a1b
SAMSUNG <SAMSUNG@SEC>
2021-01-27 16:48:46 +0900

    2021-01-27 16:48

---
+++ springtest/src/test/java/icia/kotlin/spring/DBCPTest.java
@@ -0,0 +1,44 @@
+package icia.kotlin.spring;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import icia.kotlin.spring.MapperInterface;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+
+public class DBCPTest {
+	//@Setter(onMethod_ = {@Autowired})
+	@Setter(onMethod_ = {@Autowired})
+	private DataSource data;
+	@Setter(onMethod_ = {@Autowired})
+	private SqlSessionFactory sqlSession;
+	@Setter(onMethod_ = {@Autowired})
+	private MapperInterface mapper;
+	@Test
+	public void connectTest() {
+		try{
+			SqlSession session = sqlSession.openSession();
+			Connection connect = data.getConnection();
+			log.info(session);
+			log.info(connect);
+			log.info(mapper.getDate());
+			log.info(mapper.getDate2());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
