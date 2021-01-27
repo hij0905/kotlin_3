017d433994a77f50975c0918e0f9021fbec26408
SAMSUNG <SAMSUNG@SEC>
2021-01-27 15:22:03 +0900

    2021-01-27 15:21

---
+++ spring/src/main/java/icia/kotlin/mapper/MapperInterface.java
@@ -0,0 +1,10 @@
+package icia.kotlin.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface MapperInterface {
+	@Select("SELECT SYSDATE FROM DUAL")
+	//public String getDate();
+	public String getDate2();
+	
+}
