655deebb2d272e3906be623b2ee6168ef288e72e
SAMSUNG <SAMSUNG@SEC>
2021-01-28 14:43:23 +0900

    2021.01.28 익종 홈컨트롤러, home.jsp 생성

--- springtest/src/main/java/icia/kotlin/controller/HomeController.java
+++ springtest/src/main/java/icia/kotlin/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -17,16 +18,27 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public ModelAndView home(Locale locale, ModelAndView mv) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		mv.addObject("welcome", "어서오세요~환영합니다." );
+		mv.addObject("serverTime", formattedDate );
+		
+		mv.setViewName("home");
+	
+		return mv;
 	}
+	
+	@RequestMapping(value = "/LoginForm", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView logInForm() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("loginForm");
+		return mav;
+	}
+	
 	
 }
--- springtest/src/main/webapp/WEB-INF/views/home.jsp
+++ springtest/src/main/webapp/WEB-INF/views/home.jsp
@@ -7,10 +7,8 @@
 	<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<h1>${welcome} </h1>
+	<P>  The time on the server is ${serverTime}. </P>
+	<a href="http://172.30.1.8/LoginForm">로그인폼 이동</a>
 </body>
 </html>
