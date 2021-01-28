0e012e38f399d9e2088dd18f5dc55679910dd0eb
Gaon <Gaon@asoo>
2021-01-28 17:10:42 +0900

    test

--- springtest/src/main/java/icia/kotlin/controller/HomeController.java
+++ springtest/src/main/java/icia/kotlin/controller/HomeController.java
@@ -8,25 +8,57 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import icia.kotlin.beans.Member;
 
 @Controller
 public class HomeController {
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public ModelAndView home(Locale locale, ModelAndView mv) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		mv.addObject("testWell", "한글 테스트~" );
+		mv.addObject("serverTime", formattedDate );
+		mv.setViewName("home");
+		return mv;
 	}
+	
+	@RequestMapping(value = "/LoginForm", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView loginForm() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("loginForm");
+		return mav;
+	}
+	@RequestMapping(value="/Login", method = {RequestMethod.POST})
+	public ModelAndView login(@ModelAttribute Member m, @ModelAttribute Member mInfo) {
+		ModelAndView mav = new ModelAndView();
+		System.out.println("memberInfo 값이 잘받나..?" + mInfo.getMemberInfo()[0]);
+		System.out.println("memberInfo 값이 잘받나..?" + mInfo.getMemberInfo()[1]);
+		mav.addObject("mId", m.getMId());
+		mav.addObject("mPwd", m.getMPwd());
+		mav.setViewName("loginForm");
+		return mav; 
+	}
+	//@RequestParam("memberInfo") String[] mInfo
+	
+	
+	
+//	@RequestMapping(value="/Login", method = {RequestMethod.POST})
+//	public ModelAndView login(@RequestParam("mId") String id, @RequestParam("mPwd") String pwd) {
+//		ModelAndView mav = new ModelAndView();
+//		mav.addObject("mId", id);
+//		mav.addObject("mPwd", pwd);
+//		mav.setViewName("loginForm");
+//		return mav; 
+//	}
 	
 }
